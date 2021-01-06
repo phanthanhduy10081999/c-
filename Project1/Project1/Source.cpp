15fbdcfcb546cc3438cb7ec39556597047124ebc
son <sonthaithanh99@gmail.com>
2021-01-06 18:02:57 +0700

    up len cv hinh vuong

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -12,7 +12,7 @@ void main()
 	cout << "Nhap rong= " << "\n";
 	cin >> rongv;
 	cout << "Dien tich hinh vuong la: " << rongv*rongv << "\n";
-
+	cout << "chu vi hinh vuong la: " << rongv*4 << "\n";
 	system("pause");
 
 }
