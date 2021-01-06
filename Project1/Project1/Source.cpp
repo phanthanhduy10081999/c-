566d1bb5a3f7174a715d30afc2300600f10a5eb3
phanthanhduy10081999 <nhocduy789789789@gmail.com>
2021-01-06 17:42:30 +0700

    them code

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	float dai, rong;
+	cout << "Nhap chieu dai= ";
+	cin >> dai;
+	cout << "Nhap chieu rong= ";
+	cin >> rong;
+	cout << "Dien tich hinh chu nhat la: " << dai*rong << "\n";
+	cout << "Chu vi hinh chu nhat la: " << (dai + rong) * 2;
+	system("pause");
+
+}
